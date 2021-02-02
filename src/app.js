71ec3cf9457d77e83c7075b9a263715f5cb4d041
Vukasin <vukasin.pesic@footballerista.com>
2021-02-02 08:50:04 +0100

    'update'

--- src/app.js
+++ src/app.js
@@ -12,16 +12,16 @@ export default class FFMPEG {
     };
     const ready_worker = workers.findIndex((x) => x.ready);
     if (ready_worker > -1) {
-      console.log('found ready worker')
+      console.log("found ready worker");
       workers[ready_worker]
         .process(ready_worker, file, command)
         .then(onProcess);
     } else {
-      console.log('new worker')
+      console.log("new worker");
       const new_worker = workers.push(await worker());
-      console.log('new worker ' , new_worker)
-      console.log('workers ' , workers)
-      workers[new_worker].process(ready_worker, file, command).then(onProcess);
+      workers[new_worker - 1]
+        .process(ready_worker, file, command)
+        .then(onProcess);
     }
   }
 }
